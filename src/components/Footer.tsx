5c6a584e5206977c18d17c29eb74aa9197feed6d
Kaio Nilo <kaionilofreitas@gmail.com>
2026-03-10 19:02:00 -0300

    aplicando efeito aumentando escala na logo do footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,8 +2,8 @@ import logoKyu from '../assets/logo.svg';
 export const Footer = () => {
   return (
     <footer className="w-full bg-[#F2F9FF] py-3 flex justify-center items-center border-t border-gray-200">
-      <div className="text-[#2322E3] font-black text-5xl tracking-tighter">
-        <img src={logoKyu} alt="Logo K&U" className="h-10 w-auto" />
+      <div className="flex items-center hover:scale-110 transition-transform duration-300 cursor-pointer">
+        <img src={logoKyu} alt="Logo K&U" className="h-9 w-auto" />
       </div>
     </footer>
   );
