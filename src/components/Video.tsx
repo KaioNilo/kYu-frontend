b95a0a794371c6ccf08b711c17109272aec809b8
Kaio Nilo <kaionilofreitas@gmail.com>
2026-03-10 19:45:28 -0300

    criação da base do componente do video do hero

---
+++ src/components/Video.tsx
@@ -0,0 +1,7 @@
+export const Video = () => {
+  return (
+    <div className="w-full h-[250px] bg-linear-to-b from-[#2322E3] to-[#202020]">
+
+    </div>
+  );
+};
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,13 +1,16 @@
 import { Header } from '../components/Header';
 import { Form } from '../components/Form';
 import { Footer } from '../components/Footer';
+import { Video } from '../components/video';
 
 function App() {
   return (
     <div className="min-h-screen flex flex-col bg-[#202020]">
       <Header />
       
-      <main className="grow flex flex-col items-center py-20">
+      <main>
+
+        <Video/>
         
         <Form/>
 
